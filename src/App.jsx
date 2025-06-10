a922a41cabfe4c90d585c3235f5885c14ab0e5b2
Alex Brück <alexbrueck3x@gmail.com>
2025-06-10 09:06:15 +0200

    � TradeCalculator entfernt – temporär zurückgesetzt

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import DynastyTable from './DynastyTable';
-import TradeCalculator from './components/TradeCalculator';
 
 export default function App() {
   return (
     <div className="p-4 max-w-5xl mx-auto">
       <DynastyTable />
-      <TradeCalculator />
     </div>
   );
 }
